5598104f9b767259f4af996232cfee3bda0b658d
ieuan0928 <ieuan0928@gmail.com>
2016-02-01 18:03:10 +0800

    20160201

--- EZFramework/uielements/linkButton.php
+++ EZFramework/uielements/linkButton.php
@@ -1,7 +1,30 @@
 <?php
 
 require_once("/ezframework/uielements/controlBase.php");
+require_once("/ezframework/uielements/contentControl.php");
+
 class LinkButton extends ControlBase {
+	public function __construct() {
+		$this->content = new ContentControl();
+	}
+	
+	private $content;
+	
+	public function Get($propertyName) {
+		switch (strtolower(trim($propertyName))) {
+			case "content":
+				return $this->content;
+				break;
+			default:
+				return parent::Get($propertyName);
+				break;
+		}
+	}
+	
+	public function Set($propertyName, $value) {
+		switch (strtolower(trim($propertyName))) {
+		}
+	}
 	
 }
 
