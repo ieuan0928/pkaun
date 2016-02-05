ccee0a1dc14fc312ea179134e9bc989e77cbacdd
ieuan0928 <ieuan0928@gmail.com>
2016-02-05 22:13:48 +0800

    StartSession on SiteHelper

--- EZFramework/Common/SiteHelper.php
+++ EZFramework/Common/SiteHelper.php
@@ -5,10 +5,10 @@ final class SiteHelper {
 		$phpVersion = phpversion();
 		
 		if (version_compare(phpversion(), '5.4.0') >= 0) {
-
+			if (session_status() == PHP_SESSION_NONE) session_start(); 		
 		}
-		else 
-		{
+		else { 
+			if (session_id() == '')  session_start(); 
 		}
 
 	}
