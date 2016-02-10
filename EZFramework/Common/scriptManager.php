1ff822add07d04360e7ad637233638df70e5c0ad
ieuan0928 <ieuan0928@gmail.com>
2016-02-11 03:04:18 +0800

    scriptManager partial

--- EZFramework/Common/scriptManager.php
+++ EZFramework/Common/scriptManager.php
@@ -1,7 +1,77 @@
 <?php 
 
+require_once('/ezframework/enum/scriptEmbedLocationOption.php');
+
 class ScriptManager {
 
+	private $externalScripts = Array();
+	private $inlineScripts = Array();
+	
+	private $headExternalScripts = Array();
+	private $bottomExternalScripts = Array();
+	
+	public function Get($propertyName) {
+		switch (strtolower(trim($propertyName))) {
+			case "externalscripts":
+				return $this->externalScripts;
+				break;
+			case "internalscripts":
+				return $this->inlineScripts;
+				break;
+			case "headexternalscript":
+				return $this->headExternalScripts;
+				break;
+			case "bottomexternalscript":
+				return $this->bottomExternalScripts;
+				break;
+			default:
+				die("Unable to identify Property Name.");
+				return null;
+				break;
+		}
+	}
+	
+	public function Set($propertyName, $value) {
+		switch (strtolower(trim($propertyName))) {
+			case "externalscripts":
+				$this->SegregateExternalScript($value);
+				return true;
+				break;
+			case "internalscripts":
+				$this->inlineScripts = $value;
+				return true;
+				break;
+			default:
+				die("Unable to identify Property Name.");
+				return false;
+				break;
+		}
+	}
+	
+	private function InitializeExternalScripts($value) {
+		unset($this->externalScripts);
+		unset($this->headExternalScripts);
+		unset($this->bottomExternalScripts);
+		
+		$this->externalScripts = $value;
+		$this->headExternalScripts = Array();
+		$this->bottomExternalScripts = Array();
+	}
+	
+	private function SegregateExternalScript($value) {
+		$this->InitializeExternalScripts($value);
+		
+		foreach ($this->externalScripts as $externalScript) {
+			switch ($externalScript->Get("EmbedLocation")) {
+				case ScriptEmbedLocationOption::Head:
+					array_push($this->headExternalScripts, $externalScript);
+					break;
+				case ScriptEmbedLocationOption::Bottom:
+					array_push($this->bottomExternalScripts, $externalScript);
+					break;
+			}
+		}
+	}
 }
 
 ?>
