7b405a0251b233fdb35ad8ade7bdc18af812ce6e
En Taro Michael <lelouchrebellion98@yahoo.com>
2016-02-25 04:28:35 +0800

    commit

--- EZFramework/js/ClientLinkURLHelper.js
+++ EZFramework/js/ClientLinkURLHelper.js
@@ -3,10 +3,10 @@
 		var settings = $.extend({
 			PageViewerId: ''
 		}, options);
-		alert(settings.PageViewerId + " igit sa nywa.");
+		
 		return this.each(function() {
 			$(this).click(function(e) {
-				alert(settings.PageViewerId);
+				
 				$("#" + settings.PageViewerId).html("test igit..");
 				window.history.pushState('', '', $(this).attr("href"));
 				
