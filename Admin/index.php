fc6160b2a4919c49f9555ea3dc26e38c33da28f4
ieuan0928 <ieuan0928@gmail.com>
2016-03-02 01:56:27 +0800

    require_once issue solve...
    to do:  css and js does not map correctly on subpages..

--- Admin/index.php
+++ Admin/index.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
-<script type="text/javascript" src="/ezwap_admin/js/jquery-1.11.0.min.js"></script>
-<title>Title</title>
-<style type="text/css">
-	* { padding:0; margin:0; }
-</style>
-</head>
-<body>
-	Test Shit
-</body>
-</html>
+<?php 
+
+var_dump($_SERVER);
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['CONTEXT_DOCUMENT_ROOT']); 
+
+require_once('/ezframework/site.php');
+require_once('/pages/main.php');
+
+Site::Instance()->Render(new Main());
+
+?>
