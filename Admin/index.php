40daefbecc7cd0f4fe079802e89bb606ba4ec922
En Taro Michael <lelouchrebellion98@yahoo.com>
2016-03-02 15:53:56 +0800

    commit

--- Admin/index.php
+++ Admin/index.php
@@ -1,6 +1,14 @@
 <?php 
+$include_path;
 
-set_include_path(get_include_path() . PATH_SEPARATOR . array_key_exists("CONTEXT_DOCUMENT_ROOT", $_SERVER) ? $_SERVER['CONTEXT_DOCUMENT_ROOT'] : $_SERVER['DOCUMENT_ROOT']); 
+if(array_key_exists("CONTEXT_DOCUMENT_ROOT", $_SERVER)) {
+	$include_path = $_SERVER['CONTEXT_DOCUMENT_ROOT'];
+}
+else {
+	$include_path = $_SERVER['DOCUMENT_ROOT'];
+}
+	
+set_include_path(get_include_path() . PATH_SEPARATOR . $include_path); 
 
 require_once('/ezframework/site.php');
 require_once('/pages/main.php');
