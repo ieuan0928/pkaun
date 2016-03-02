d3456d908c3194f9d1f408b72d0dbb9dc06f2248
ieuan0928 <ieuan0928@gmail.com>
2016-03-02 15:50:33 +0800

    SERVER array_key_exists CONTEXT_DOCUMENT_ROOT

--- Admin/index.php
+++ Admin/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-set_include_path(get_include_path() . PATH_SEPARATOR . isset($_SERVER['CONTEXT_DOCUMENT_ROOT']) ? $_SERVER['CONTEXT_DOCUMENT_ROOT'] : $_SERVER['DOCUMENT_ROOT']); 
+set_include_path(get_include_path() . PATH_SEPARATOR . array_key_exists("CONTEXT_DOCUMENT_ROOT", $_SERVER) ? $_SERVER['CONTEXT_DOCUMENT_ROOT'] : $_SERVER['DOCUMENT_ROOT']); 
 
 require_once('/ezframework/site.php');
 require_once('/pages/main.php');
