e698b072d3dd7468eb3c954c3d445e1790076c44
ieuan0928 <ieuan0928@gmail.com>
2016-03-10 22:00:55 +0800

    Implementation Started for Nested Page Viewer
    Criteria:
    Redundancy of JS and CSS must be resolved.

--- pages/home.php
+++ pages/home.php
@@ -2,6 +2,8 @@
 
 require_once('/ezframework/uielements/pageBase.php');
 require_once('/ezframework/uielements/label.php');
+require_once('/ezframework/uielements/pageViewer.php');
+require_once('/ezframework/common/urlParameterMapper.php');
 
 class Home extends PageBase {
 	public function __construct() {
@@ -12,15 +14,26 @@ class Home extends PageBase {
 	
 	private $contentPageViewer;
 	private $labeltest;
+	private $storeURLParameter;
 	
 	private function InitializeElements() {
 		$this->labeltest = new Label();
+		$this->contentPageViewer = new PageViewer();
+		$this->storeURLParameter = new URLParameterMapper();
 	}
 	
 	public function CreateElements() {
 		$this->labeltest->Set("identifier", "label_test");
 		$this->labeltest->Set("Content", "label test lang!!");
 		$this->labeltest->Set("Parent", $this);
+		
+		$this->storeUrlParameter->Set("URLValue", "Stores");
+		$this->storeUrlParameter->Set("PageTypeName", "Stores");
+		$this->storeUrlParameter->Set("PagePath", "/pages/stores.php");
+		
+		$this->contentPageViewer->Set("Parent", $this);
+		$this->contentPageViewer->Set("Identifier", "homePageViewer");
+		$this->contentPageViewer->Set("PostVariable", "HomePage");
 		
 	}
 }
